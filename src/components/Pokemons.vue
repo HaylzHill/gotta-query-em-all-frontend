a05e1817129c461fe54d4af9e5787cec67d1cd7b
Jaysc <jaysc2@gmail.com>
2020-06-20 00:06:41 +0100

    More playing around

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,18 +1,24 @@
 <template>
-  <p>{{ pokemons }}</p>
+  <div id="wrapper">
+    <Pokemon v-for="pokemon in pokemons" :pokemon="pokemon" />
+  </div>
 </template>
 
 <script>
+import Pokemon from "./Pokemon.vue";
 import { execute, makePromise } from "apollo-link";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
 
 export default {
   name: "Pokemons",
+  components: {
+    Pokemon
+  },
   props: {},
   data() {
     return {
-      pokemons: null,
+      pokemons: null
     };
   },
   created() {
@@ -24,15 +30,26 @@ export default {
         query {
           pokemon {
             name
+            imgSrc
           }
         }
       `,
-      variables: {}, //optional
+      variables: {} //optional
     };
 
     makePromise(execute(link, operation))
-      .then((data) => (this.pokemons = data))
-      .catch((error) => console.log(`received error ${error}`));
-  },
+      .then(data => {
+        console.log(data.data.pokemon);
+        return (this.pokemons = data.data.pokemon);
+      })
+      .catch(error => console.log(`received error ${error}`));
+  }
 };
 </script>
+
+<style scoped>
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>>
