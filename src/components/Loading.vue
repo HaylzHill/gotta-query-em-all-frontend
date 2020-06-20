f9157667a0d56a5fad5942702b3f225bcc4eeb4e
Jaysc <jaysc2@gmail.com>
2020-06-20 18:27:00 +0100

    Added a walking pikachu to loading bar

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="nes-container with-title nice-width">
-    <p class="title">
-      Loading...
-    </p>
-    <p>{{ msg }}</p>
-    <progress
-      class="nes-progress is-primary"
-      max="100"
-      :value="percentage"
-    />
+    <img
+      src="../assets/pikachu-walking.gif"
+      alt="Pikachu walking"
+      height="50"
+    >
+    <div>
+      <p class="title">
+        Loading...
+      </p>
+      <p>{{ msg }}</p>
+      <progress
+        class="nes-progress is-primary"
+        max="100"
+        :value="percentage"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { ref, watchEffect } from "vue";
-import useLoading from '../use/loading';
+import useLoading from "../use/loading";
 
 const funMessage = (percentage) => {
   if (percentage.value < 30) {
@@ -25,7 +32,7 @@ const funMessage = (percentage) => {
     return "Receiving Pokemon data...";
   }
 
-  return"Sending to Pokedex...";
+  return "Sending to Pokedex...";
 };
 
 export default {
@@ -35,17 +42,17 @@ export default {
     const { percentage } = useLoading();
 
     watchEffect(() => {
-      msg.value = funMessage(percentage)
+      msg.value = funMessage(percentage);
     });
 
     return { msg, percentage };
-  }
+  },
 };
 </script>
 
 <style scoped>
-  .nice-width {
-    margin: auto;
-    max-width: 500px;
-  }
+.nice-width {
+  margin: auto;
+  max-width: 500px;
+}
 </style>
